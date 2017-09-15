d6d9d371d851e2f4a7fce46724aa7d5eef40006b
aliir74 <aliirani74@gmail.com>
2017-09-15 11:58:49 +0430

    try to get name and country

--- main.py
+++ main.py
@@ -4,13 +4,26 @@ import requests
 from selenium import webdriver
 
 
-
 url = 'https://www.topuniversities.com/university-rankings/university-subject-rankings/2017/architecture'
-page = requests.get(url).text
-soup = BeautifulSoup(page, 'html.parser')
-table = soup.find(id='qs-rankings')
-print(soup.prettify())
+url2 = 'https://www.topuniversities.com/university-rankings/university-subject-rankings/2017/computer-science-information-systems'
 
 browser = webdriver.Chrome("/home/ali/Software/chromedriver")
 browser.set_window_size(1366, 638)
 browser.set_page_load_timeout(120)
+browser.get(url)
+soup = BeautifulSoup(browser.page_source, 'html.parser')
+table = soup.find(id='qs-ranking')
+unis = soup.find_all('tbody')[0].find_all('tr')
+list = []
+for i in unis:
+    rank = i.findChildren('span', {'class': 'rank'})[0].text
+    if(rank[0] == '='):
+        rank = rank[1:]
+    name = i.findChildren('a', {})[0].text
+    #country = i.findChildren('img')[0]['data-original-title']
+    country = None
+
+    d = {'name': name, 'country': country, 'rank': rank}
+    list.append(d)
+    print(d)
+browser.quit()
