ffbef74b7221c792ea6c036e72ca6efcaf5a9f1a
aliir74 <aliirani74@gmail.com>
2017-09-15 11:21:59 +0430

    add selenium chrome driver

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from bs4 import BeautifulSoup
 import csv
 import requests
+from selenium import webdriver
+
 
 
 url = 'https://www.topuniversities.com/university-rankings/university-subject-rankings/2017/architecture'
 page = requests.get(url).text
 soup = BeautifulSoup(page, 'html.parser')
 table = soup.find(id='qs-rankings')
-print(table)
+print(soup.prettify())
+
+browser = webdriver.Chrome("/home/ali/Software/chromedriver")
+browser.set_window_size(1366, 638)
+browser.set_page_load_timeout(120)
